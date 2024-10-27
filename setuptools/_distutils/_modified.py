20d4d179557ba469970e9254fe28187458b62218
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-10-27 10:47:28 +0800

    alvistack/v75.9.1
        git clean -xdf
        tar zcvf ../python-setuptools_75.9.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-setuptools.spec ../python-setuptools_75.9.1-1.spec
        cp ../python-setuptools*75.9.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/pypa-setuptools-75.9.1/
        rm -rf ../python*-setuptools*75.9.1*.* ../python*-pkg-resources_75.9.1*.*
    
    See https://github.com/pypa/setuptools/pull/4600
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setuptools/_distutils/_modified.py
+++ setuptools/_distutils/_modified.py
@@ -3,7 +3,7 @@
 import functools
 import os.path
 
-from jaraco.functools import splat
+from setuptools._vendor.jaraco.functools import splat
 
 from .compat.py39 import zip_strict
 from .errors import DistutilsFileError
--- setuptools/_distutils/util.py
+++ setuptools/_distutils/util.py
@@ -17,7 +17,7 @@ import sys
 import sysconfig
 import tempfile
 
-from jaraco.functools import pass_none
+from setuptools._vendor.jaraco.functools import pass_none
 
 from ._log import log
 from ._modified import newer
